c63ae6f8b059e943aaa490e1acab096e3b4bd213
washington <washingtonrissarodrigues@gmail.com>
2024-06-14 17:06:52 -0300

    prova de logica

---
+++ exerc01.py
@@ -0,0 +1,5 @@
+# Exercício 1: Saudação
+
+nome = input("Por favor, insira seu nome: ")
+
+print("Olá, " + nome + " Seja bem vindo" )
---
+++ exerc02.py
@@ -0,0 +1,5 @@
+# Exercício 2: Impressão de números ímpares
+
+for n in range(1, 102):
+    if n % 2-1 == 0:
+        print(n, end=' ')
